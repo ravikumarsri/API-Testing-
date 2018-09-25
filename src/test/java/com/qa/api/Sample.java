4acd1fd987dfbdce5c05da14d6a17ab5b2bfe6dc
ravikumarsri <ds.sridhar09@gmail.com>
2018-09-25 13:09:11 +0800

    new code commit into branch

--- src/test/java/com/qa/api/Sample.java
+++ src/test/java/com/qa/api/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample program");
-
+		System.err.println("welcom");
 	}
 
 }
