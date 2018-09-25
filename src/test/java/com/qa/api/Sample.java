7f4e6fec77b521971a44790a3f3370c80d9452f4
ravikumarsri <32927618+ravikumarsri@users.noreply.github.com>
2018-09-25 16:42:43 +0800

    Update Sample.java

--- src/test/java/com/qa/api/Sample.java
+++ src/test/java/com/qa/api/Sample.java
@@ -5,11 +5,6 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample program");
-System.out.println("hello");
-		System.err.println("welcom");
-	
-		System.out.println("sridhar and mamta ");
-System.out.println("onlyh for master ");
 
 	}	
 
