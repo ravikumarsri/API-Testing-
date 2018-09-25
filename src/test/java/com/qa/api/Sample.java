795c9ebd09a5061866231822d1f4afdb36c357ea
ravikumarsri <ds.sridhar09@gmail.com>
2018-09-25 13:01:50 +0800

    add into branch

--- src/test/java/com/qa/api/Sample.java
+++ src/test/java/com/qa/api/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample program");
-
+System.out.println("hello");
 	}
 
 }
