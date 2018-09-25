5b320944dc86f27c564c10d99024312fbe527e9d
ravikumarsri <ds.sridhar09@gmail.com>
2018-09-25 14:05:38 +0800

    added the code

--- src/test/java/com/qa/api/Sample.java
+++ src/test/java/com/qa/api/Sample.java
@@ -5,9 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample program");
-<<<<<<< HEAD
 System.out.println("hello");
-=======
 		System.err.println("welcom");
 		
 		
@@ -18,7 +16,7 @@ System.out.println("hello");
 		System.err.println("welcom");
 		System.err.println("welcom");
 		System.err.println("welcom");
->>>>>>> practicecode
+
 	}
 
 }
